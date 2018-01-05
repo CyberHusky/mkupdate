c3a70272d8e1d626bcb21ad73e81a295473bf140
Unknown <gordieiev.artem@gmail.com>
2018-01-05 19:03:56 +0200

    v1.001
    test commit

--- mkupdate.m
+++ mkupdate.m
@@ -5,7 +5,24 @@
 clc
 clear
 
+prog_name = 'myprogram';
+web = 'https://raw.githubusercontent.com/CyberHusky/mkupdate/master/current_version.txt';
+download_web = 'https://github.com/CyberHusky/mkupdate/archive/master.zip';
+
 cur_local_ver = cell2mat( importdata([pwd, filesep, 'current_version.txt']) );
-cur_local_ver = str2double(cur_local_ver(2:end));
+cur_local_ver = str2double( cur_local_ver(2:end) );
 
 
+cur_gl_ver = cell2mat ( cellstr( webread(web) ) );
+cur_gl_ver = str2double( cur_gl_ver(2:end) );
+% https://github.com/CyberHusky/mkupdate/archive/master.zip
+
+
+% websave([prog_name,'.zip'], download_web)
+unzip(download_web)
+
+if cur_gl_ver > cur_local_ver
+    
+    
+    
+end
