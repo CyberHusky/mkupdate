bf85b9cd8e42fd4bfe67a536765e501def9af303
Unknown <gordieiev.artem@gmail.com>
2018-01-05 20:45:49 +0200

    v1.1
    function ready to use for open github server

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# mkupdate
-Function for remote update of personal MatLab files
+## mkupdate
+# Function for remote update of personal MatLab files.
+
+MKUPDATE is simple function for remote programm files update from 
+git-server to end user. It will be useful for your end user to make 
+update of current version of your program.
+
+OUTPUT:
+    this m-file will scan your specified github repository, check program 
+    version, download files if needed and change dowloaded file in current folder. 
+
+INPUT:
+   prog_name - name of you program on github (example: 'mkupdate')
+   web - http site of raw version of "current_version" file (example: 'https://raw.githubusercontent.com/CyberHusky/mkupdate/master/current_version.txt')
+   download_web - http site of zip archive of your program on github (example: 'https://github.com/CyberHusky/mkupdate/archive/master.zip')
+
--- mkupdate.m
+++ mkupdate.m
@@ -1,13 +1,17 @@
-% mkupdate - function for remote programm files update from git-server
-% filesep - separator for current OS version (win, lin etc.)
-% computer - information about PC witch MatLab run (https://www.mathworks.com/help/matlab/ref/computer.html)
+function mkupdate(prog_name, web, download_web)
 
-clc
-clear
-
-prog_name = 'mkupdate';
-web = 'https://raw.githubusercontent.com/CyberHusky/mkupdate/master/current_version.txt';
-download_web = 'https://github.com/CyberHusky/mkupdate/archive/master.zip';
+% MKUPDATE is simple function for remote programm files update from 
+% git-server to end user. It will be useful for your end user to make 
+% update of current version of your program.
+%
+% OUTPUT:
+%    this m-file will scan your specified github repository, check program 
+%    version, download files if needed and change dowloaded file in current folder. 
+%
+% INPUT:
+%   prog_name - name of you program on github (example: 'mkupdate')
+%   web - http site of raw version of "current_version" file (example: 'https://raw.githubusercontent.com/CyberHusky/mkupdate/master/current_version.txt')
+%   download_web - http site of zip archive of your program on github (example: 'https://github.com/CyberHusky/mkupdate/archive/master.zip')
 
 cur_local_ver = cell2mat( importdata([pwd, filesep, 'current_version.txt']) );
 cur_local_ver = str2double( cur_local_ver(2:end) );
@@ -15,27 +19,22 @@ cur_local_ver = str2double( cur_local_ver(2:end) );
 
 cur_gl_ver = cell2mat ( cellstr( webread(web) ) );
 cur_gl_ver = str2double( cur_gl_ver(2:end) );
-% https://github.com/CyberHusky/mkupdate/archive/master.zip
 
 
-% websave([prog_name,'.zip'], download_web)
-
-
-% movefile ([pwd, filesep, prog_name, '-master'], pwd)
-% movefile /home/artem/GitHub/mkupdate/mkupdate-master/begin_test.m /home/artem/GitHub/mkupdate/begin_test.m
-% copyfile ([pwd, filesep, prog_name, '-master'], pwd)
 
 if cur_gl_ver > cur_local_ver
     
-    zip([prog_name,'.bak'], pwd)
+    zip([prog_name,'.bak'], pwd) % make backup files in zip
     
-    unzip(download_web)
+    unzip(download_web) % alternative - websave([prog_name,'.zip'], download_web)
 
     file = dir([pwd, filesep, prog_name, '-master']);
     for i=3:length(file)
         status = movefile([file(i).folder, filesep, file(i).name], [pwd, filesep, file(i).name]);
     end
-    rmdir([pwd, filesep, prog_name, '-master'])
+    rmdir([pwd, filesep, prog_name, '-master']) % delete buffer folder
     
+else
+    errordlg(['Current version of program: ',cellstr(webread(web))],'Update is not necessary!')
     
 end
